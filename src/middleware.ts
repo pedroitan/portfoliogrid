e69edc22b39ca34501ec74a938a66f3f6ccb7711
pedroitan <pedroitan@gmail.com>
2026-04-11 16:05:19 -0300

    fix: remove unused NextResponse import from middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,5 @@
 import createMiddleware from 'next-intl/middleware';
 import { routing } from './i18n/routing';
-import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 const intlMiddleware = createMiddleware(routing);
