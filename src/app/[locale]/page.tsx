afe51f34512b94c003cc0ff2357477079573d529
pedroitan <pedroitan@gmail.com>
2026-04-13 13:46:46 -0300

    fix: remove unused imports ExpertiseSwipeContainer and ExpertiseDescriptions

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,4 @@
 import HeroVideoDynamic from '@/components/HeroVideoDynamic';
-import ExpertiseSwipeContainer from '@/components/ExpertiseSwipeContainer';
-import ExpertiseDescriptions from '@/components/ExpertiseDescriptions';
 import VideoGrid from '@/components/VideoGrid';
 import Bio from '@/components/Bio';
 import Contact from '@/components/Contact';
